65e11bd0d44f485c1b709bfb35c6ac8819ecac12
Denis Starkov <starkden1000@gmail.com>
2025-03-14 09:57:55 +0300

    created navigation bar below (created package (tabs) for this), package movies for later

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {Tabs} from 'expo-router'
+
+const Saved = () => {
+  return (
+    <View>
+      <Text>saved</Text>
+    </View>
+  )
+}
+
+export default Saved
