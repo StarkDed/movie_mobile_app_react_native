030fe23ab5ff8902b6f025debf731acc9e49a8f6
Denis Starkov <starkden1000@gmail.com>
2025-03-22 22:32:05 +0300

    created file that connected to TMDB to fetch movies ,created hook (useFetch ) to fetch conveniently data and checked connection in index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,19 @@
-import {Text,View,Image,ScrollView} from "react-native"
+import {Text,View,Image,ScrollView,ActivityIndicator} from "react-native"
 import { images } from "@/constants/images"
 import { icons } from "@/constants/icons"
 import SearchBar from "../../components/SearchBar"
 import {useRouter} from "expo-router"
+import useFetch from "@/services/useFetch"
+import { fetchMovies } from "@/services/api"
 
 export default function Index(){
   const router = useRouter()
+  const {data:movies,
+    loading:moviesLoading,
+    error:moviesError}
+     = useFetch(()=>
+      fetchMovies({query:''})
+     )
 
   return (
     <View className="flex-1 bg-primary" >
@@ -24,14 +32,29 @@ export default function Index(){
         <Image 
           source={icons.logo}
           className="w-12 h-10 mt-20 mb-5 mx-auto"
-          
         />
-        <View>
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder="Search for the movie"
-           />
-        </View>
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
+          />
+        ) : moviesError ? (
+          <Text className="text-lg text-white font-bold mt-5 mb-3">Error:{moviesError?.message}</Text>
+        ) : (
+          <View>
+            <SearchBar
+              onPress={()=> router.push('/search')}
+              placeholder="Search for movie "
+            />  
+            
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">Latest movie</Text>
+            </>
+          </View>
+        )}
+        
       </ScrollView>
     </View>
   )
