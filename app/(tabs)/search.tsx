527c02aeef57381d10ff27cfb1f56c9b84bcd987
Denis Starkov <starkden1000@gmail.com>
2025-03-25 21:11:06 +0300

    added same style like in index to search

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,67 @@
-import { View, Text } from 'react-native'
+import { View, Image,FlatList, ActivityIndicator } from 'react-native'
 import React from 'react'
+import { images } from '@/constants/images'
+import MovieCard from '@/components/MovieCard'
+import useFetch from '@/services/useFetch'
+import { fetchMovies } from '@/services/api'
+import { icons } from '@/constants/icons'
+import SearchBar from '@/components/SearchBar'
 
 const Search = () => {
+  const {data:movies,
+    loading:moviesLoading,
+    error:moviesError
+  } = useFetch(()=>fetchMovies({query:''}))
+
+  
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image 
+        source={images.bg}
+        className="flex-1 absolute z-0 w-full"
+        resizeMode="cover"
+      /> 
+      <FlatList 
+        data={movies}
+        renderItem={({item})=>(
+          <MovieCard
+            {...item}
+          />
+        )}
+        className=" px-5 mt-5"
+        keyExtractor={(item)=>item.id.toString()}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent:'center',
+          alignItems:'center',
+          gap:16,
+          marginVertical:16,
+        }}
+        contentContainerStyle={{
+          paddingBottom:100
+        }}
+        ListHeaderComponent={
+          <>
+          <View className="w-full flex-row justify-center mt-20 items-center">
+            <Image 
+              source={icons.logo}
+              className="w-12 h-10"
+            />
+          </View>
+
+          <View className="my-5">
+            <SearchBar placeholder="Search movies ..." />
+          </View>
+
+          {moviesLoading && 
+            <ActivityIndicator
+              size="large"
+              color="#0000ff"
+            />}
+          </>
+        }
+      />
     </View>
   )
 }
