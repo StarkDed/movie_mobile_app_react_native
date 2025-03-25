1e84348f370922f1d2abca7bfc96d1f11cfc3cce
Denis Starkov <starkden1000@gmail.com>
2025-03-25 18:48:58 +0300

    added some more details about movie on screen

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,12 +1,13 @@
 import { View, Text, TouchableOpacity,Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { icons } from '@/constants/icons'
 
 const MovieCard = ({id,poster_path,title,vote_average,release_date}:Movie) => {
   return (
     
         <Link
-            href={`/movie/${id}`}
+            href={`/movies/${id}`}
             asChild
         >
             <TouchableOpacity
@@ -21,7 +22,32 @@ const MovieCard = ({id,poster_path,title,vote_average,release_date}:Movie) => {
                     className="w-full h-52 rounded-lg"
                     resizeMode="cover"
                 />
-                <Text className="font-bold text-white text-sm">{title}</Text>
+                <Text 
+                className="font-bold text-white text-sm mt-1"
+                numberOfLines={1}
+                >
+                    {title}
+                </Text>
+
+                <View className="flex-row items-center justify-between mb-1">
+                    <View className='flex-row justify-start gap-x-1'>
+                        <Image
+                            source={icons.star}
+                            className="size-4"
+                        />
+                        <Text className="text-white text-xs">
+                            {Math.round(vote_average*10)/10}
+                        </Text>
+                    </View>
+                    
+                    <View className="justify-end">
+                        <Text className="text-xs text-light-300 font-bold">
+                            {release_date?.split('-')[0]}
+                        </Text>
+                    </View>
+                </View>
+
+                
             </TouchableOpacity>
         </Link>
     
